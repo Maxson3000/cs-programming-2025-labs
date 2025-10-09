7a55644c0dc334a5b81d08045d09f8bde89b2795
Maxson3000 <zilmax1608@bk.ru>
2025-10-09 20:49:14 +1000

    Vtt

---
+++ lab1/абораторная.py
@@ -0,0 +1,71 @@
+'''
+a=555
+b=11.111
+q="MAx"
+w=1+1=2
+'''
+
+name="MAKSIM"
+age=18
+print(name,age)
+'''
+a=5
+b=3
+P=(a+b)*2
+S=a*b
+print(P,S)
+
+sh=3
+dl=8
+print((sh+(4*dl))*(sh-(3*dl))+sh**2)
+
+pe=342
+oe=56.2
+sa='43'
+fs=(int(sa))+pe+oe
+print(fs)
+
+
+print('* * *')
+print(  '* * * ')
+print(   ' * *  ')
+'''
+a=1
+b=2
+print((a-b),(a+b),(a*b),(a/b),(a**b),(a//b),(a%b,(a<b),(a>b),(a==b),(a!=b),(a<=b),(a>=b)))
+
+
+print(f'Меня зовут {name} мой возраст {age}')
+
+
+w1="Съешь"
+w2="еще этих"
+w3="мягких французских"
+w4="булок"
+w5="да"
+w6="выпей"
+w7="чаю"
+predlogenie=w1+w2+w3+w4+w5+w6+w7
+print(predlogenie)
+
+
+stroka='нет! да!'
+print(stroka*4)
+
+
+
+input_data=input()
+numbers=input_data.split(',')
+a1=int(numbers[0].strip())
+b1=int(numbers[1].strip())
+c=int(numbers[2].strip())
+r=(a1+c)//b1
+print(f"Результат вычислений{r}")
+
+w=input()
+print(w[:4])
+print(w[-2])
+print(w[4:8])
+print(w[::-1])
+
+
